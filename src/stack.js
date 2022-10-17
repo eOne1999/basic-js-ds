a07791bafeb9ab9ead94251abc9d5fde9a60dd82
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2022-10-18 01:50:40 +0300

    feat: implement stack solution

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.stack[this.stack.length - 1];
+    this.stack.splice(this.stack.length - 1, 1);
+    return result;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
